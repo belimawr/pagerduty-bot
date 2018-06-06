02b9587b9aca61a08269e971e9e98292ed81e2d6
Tiago Queiroz <contato@tiago.eti.br>
2018-06-06 21:22:48 +0200

    Better interfaces and comments

--- reporter/reporter.go
+++ reporter/reporter.go
@@ -6,23 +6,38 @@ import (
 	"time"
 )
 
+// Reporter - Generates the report
 type Reporter interface {
-	AddDayForUser(user string, day time.Time)
-	//	AddTimeForUser(user string, day time.Time, time time.Duration)
 	Report(io.Writer)
 }
 
+// Store - Interface to store the data
+type Store interface {
+	AddDayForUser(user string, day time.Time)
+	//	AddTimeForUser(user string, day time.Time, time time.Duration)
+}
+
+// StoreReporter - a Store that also generates Reports
+type StoreReporter interface {
+	Store
+	Reporter
+}
+
+// DayTyper - indentifies the type of the day
 type DayTyper interface {
 	Type(time.Time) string
 }
 
+// DayTyperFunc - functions can be of type DayTyper
 type DayTyperFunc func(time.Time) string
 
+// Type - wraps a function to implement DayTyper
 func (f DayTyperFunc) Type(t time.Time) string {
 	return f(t)
 }
 
-func New() Reporter {
+// New - returns an implementation of StoreReporter
+func New() StoreReporter {
 	return &inMemory{
 		m:     map[string]onCallReport{},
 		typer: DayTyperFunc(dayTyperFn),
