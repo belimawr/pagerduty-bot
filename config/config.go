4ff303b46d0004d360cc0313ad60f66c6f66a6ab
Tiago Queiroz <contato@tiago.eti.br>
2018-06-06 21:10:25 +0200

    Adding log level

--- config/config.go
+++ config/config.go
@@ -11,12 +11,14 @@ import (
 type Config struct {
 	AccessToken       string   `env:"ACCESS_TOKEN" envDefault:""`
 	LogOutput         string   `env:"LOG_OUTPUT" envDefault:"console"`
+	LogLevel          string   `env:"LOG_LEVEL" envDefault:"debug"`
 	TimeZone          string   `env:"TIMEZONE" envDefault:"UTC"`
 	EscalationPlocies []string `env:"ESCALATION_POLICIES" envDefault:"" envSeparator:","`
 }
 
 // Logger - returns a logger configured using the parameters in the struct
 func (c Config) Logger() zerolog.Logger {
+	zerolog.SetGlobalLevel(strToZerologLevel(c.LogLevel))
 	logger := zerolog.New(os.Stderr).With().
 		Timestamp().
 		Logger()
@@ -26,4 +28,23 @@ func (c Config) Logger() zerolog.Logger {
 	}
 
 	return logger
+}
+
+func strToZerologLevel(level string) zerolog.Level {
+	switch strings.ToUpper(level) {
+	case "DEBUG":
+		return zerolog.DebugLevel
+	case "INFO":
+		return zerolog.InfoLevel
+	case "WARN":
+		return zerolog.WarnLevel
+	case "ERROR":
+		return zerolog.ErrorLevel
+	case "FATAL":
+		return zerolog.FatalLevel
+	case "PANIC":
+		return zerolog.PanicLevel
+	default:
+		return zerolog.InfoLevel
+	}
 }
